256ae3fad5b5296ae0f6035e7a10df9d81beb2c1
Hendrik Schonlebe <hendrik@unitysoftware.com.au>
2020-04-06 10:54:35 +1000

    First Commit

--- UNITYPCoatData.cs
+++ UNITYPCoatData.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
+
 
 namespace UNITYPCoatData
 {
     public class UNITYPCoatData
     {
+        #region Global Variables
+        public SqlConnection PCConnection { get; set; }
+        public String ErrorMessage { get; set; } = string.Empty;
+        #endregion
         #region Paint Product Table
         #region Colour Groups
         public Int32 ColourGroupId { get; set; } = -1;
         public String ColourGroupDescription { get; set; } = string.Empty;
         public DataTable ColourGroupRecord { get; set; } = new DataTable();
         public DataTable ColourGroupRecords { get; set; } = new DataTable();
+        public Boolean Insert_Colour_Group(SqlTransaction TRNEnvelope)
+        {
+            Boolean isSuccessful = true;
+
+            ErrorMessage = string.Empty;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return isSuccessful;
+        }
 
         #endregion
         #endregion
