e8027cfd7dbbd2c0a9e1147b20a9b51844d3fb4c
Hendrik Schonlebe <hendrik@unitysoftware.com.au>
2020-04-06 10:39:03 +1000

    Add project files.

---
+++ UNITYPCoatData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace UNITYPCoatData
+{
+    public class UNITYPCoatData
+    {
+        #region Paint Product Table
+        #region Colour Groups
+        public Int32 ColourGroupId { get; set; } = -1;
+        public String ColourGroupDescription { get; set; } = string.Empty;
+        public DataTable ColourGroupRecord { get; set; } = new DataTable();
+        public DataTable ColourGroupRecords { get; set; } = new DataTable();
+
+        #endregion
+        #endregion
+
+    }
+}
